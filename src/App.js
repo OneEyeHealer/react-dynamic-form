47a397aad3a08d3a9074189a4632beec9b19964b
Harsh Arora <46344612+OneEyeHealer@users.noreply.github.com>
2021-06-18 08:50:52 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [data, setdata] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const { name, email, message } = data;
+  const handleChange = (e) => {
+    setdata({ ...data, [e.target.name]: e.target.value });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(
+        "https://v1.nocodeapi.com/oneeyehealer/google_sheets/TwOsMmxQmtOiiMKP?tabId=Sheet1",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify([
+            [name, email, message, new Date().toLocaleString()],
+          ]),
+        }
+      );
+      await response.json();
+      setdata({ ...data, name: "", email: "", message: "" });
+    } catch (error) {}
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form onSubmit={handleSubmit}>
+          <h3>Feedback Form</h3>
+          <div class="form-group">
+            <label for="" htmlFor="name">
+              Name
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              name="name"
+              value={name}
+              onChange={handleChange}
+            />
+          </div>
+          <div class="form-group">
+            <label for="" htmlFor="email">
+              Email
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              name="email"
+              value={email}
+              onChange={handleChange}
+            />
+          </div>
+          <div class="form-group">
+            <label for="" htmlFor="message">
+              Message
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              name="message"
+              value={message}
+              onChange={handleChange}
+            />
+          </div>
+          <div class="text-center my-3">
+            <input type="submit" value="Submit" className="btn btn-primary" />
+          </div>
+        </form>
       </header>
     </div>
   );
-}
+};
 
 export default App;
